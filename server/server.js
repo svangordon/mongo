cc5b10589db5000a0e93dd478808b3dcd5541b41
Student <student@RU-003.local>
2016-03-08 12:34:48 -0700

    included a silly privacy policy so that I could get on the insta API

--- server/server.js
+++ server/server.js
@@ -26,6 +26,10 @@ app.get('/', function (req, res) {
 	res.sendFile('index.html', {root : '../client/html'})
 })
 
+app.get('/privacy', function (req, res) {
+	res.sendFile('privacy.html', {root : '..client/html'})
+})
+
 
 app.listen(3000, function() {
   console.log('Listening on port 3000...')
