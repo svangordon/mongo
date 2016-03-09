3d24512cdebba486691e04a78e18ecfa9e798988
Student <student@RU-003.local>
2016-03-08 19:01:11 -0700

    ugh

--- client/angular/filters/filters.js
+++ client/angular/filters/filters.js
@@ -6,9 +6,10 @@ angular.module('app')
 			var schedStart = moment(schedule.firstHour())
 			var schedEnd = moment(schedule.lastHour());
 			// emissions.forEach(function (cur) {console.log(moment(cur.end).format())})
-			return emissions.filter(function(cur) { return moment(cur.start).isBefore(schedEnd)
-														&& moment(cur.end).isAfter(schedStart) })
-			// return out
+			if (emissions) {
+				return emissions.filter(function(cur) { return moment(cur.start).isBefore(schedEnd)
+															&& moment(cur.end).isAfter(schedStart) })
+			}// return out
 		}
 	})
 	.filter('daysInView', function () {
